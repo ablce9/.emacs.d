aef4344bb8be0111b6b8fa648f8ce51bae66c494
xun <lu.xun@gmx.com>
2017-12-07 07:19:43 +0900

    Initial commit

---
+++ init.el
@@ -0,0 +1,87 @@
+;;; package -- init.el
+
+;;; Code:
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(global-set-key (kbd "C-c i")
+		(lambda() (interactive) (load-file "~/.emacs.d/init.el")))
+(global-set-key (kbd "C-c p")
+		(lambda() (interactive) (load-file "~/.bashrc")))
+
+(define-key global-map (kbd "C-c q") 'replace-regexp)
+
+(set-face-attribute 'default nil :height 50)
+(setq linum-format "%3d\u2502")
+(line-number-mode)
+(global-linum-mode 1)
+(setq column-number-mode t)
+
+;; control auto-save-list
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+
+(setq backup-directory-alist'(("."."~/.backups")))
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+
+; theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'jqg t)
+
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "red")
+(set-face-foreground 'highlight nil)
+
+(defun open-nice()
+  "this macro lets you open window nice"
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+		     (file-name-directory (buffer-file-name))
+		   default-directory))
+	 (height (/ (window-total-height) 3))
+	 (name (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    ;; (rename-buffer (concat "*opened: " name "*"))
+    )
+  )
+
+(global-set-key (kbd "C-c o") 'open-nice)
+
+(defun eshell-here()
+  "Opens up a new shell in the directory associated with the 
+current buffer's file. The eshell is renamed to match that directory
+to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+		     (file-name-directory (buffer-file-name))
+		   default-directory))
+	 (height (/ (window-total-height) 3))
+	 (name (car (last (split-string parent "/" t)))))
+  (split-window-vertically (- height))
+  (other-window 1)
+  (eshell "new")
+  (rename-buffer (concat "*eshell: " name "*"))
+  
+  (insert (concat "ls"))
+  (eshell-send-input)))
+
+(global-set-key (kbd "C-c e") 'eshell-here)
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window)
+  ;; eshell-display-predicate-help
+  )
+
+;;; init.el ends here
---
+++ themes/jqg-theme.el
@@ -0,0 +1,45 @@
+;;; jqg-theme.el
+
+;;; jqg-theme
+
+;;; Code:
+(deftheme jqg
+  "created 2015-01-20")
+ 
+(custom-theme-set-faces
+ `jqg
+ `(bold ((t (:bold t)))) 
+ `(bold-italic ((t (:bold t))))
+ `(border-glyph ((t (nil))))
+ `(fringe ((t (:background "#a4c2cc"))))
+;;   `(mode-line ((t (:foreground "#072d40" :background "#99bac7"))))
+
+ ;; Custom theme and more customed by me.
+ ;; Region
+ `(region ((t (:background "MidnightBlue"))))
+ ;; `(region ((t (:background "#356a9c"))))
+
+ ;; Cursor
+ `(cursor ((t (:foregound "#000000"))))
+ 
+  `(secondary-selection ((t (:background "dodger blue"))))
+ `(linum ((t (:background "black" :foreground "#99bac7"))))
+ ;; For comments
+ '(font-lock-comment-delimiter-face ((t (:inherit (font-lock-comment-face)))))
+ '(font-lock-comment-face ((t (:foreground "#00ff00" :slant italic))))
+ 
+ `(font-lock-builtin-face ((t (:foreground "#ff00ff"))))
+ `(font-lock-comment-face ((t (:foreground "#575b5b"))))
+ `(font-lock-function-name-face ((t (:foreground "#ec9346"))))
+;;  `(font-lock-keyword-face ((t (:foreground "#a4cee5")))) blue
+  `(font-lock-string-face ((t (:foreground "#e8b778"))))
+  `(font-lock-type-face ((t (:foreground"#74abbe"))))
+  `(font-lock-constant-face ((t (:foreground "#eeedec"))))
+  `(font-lock-variable-name-face ((t (:foreground "#ff0ff"))))
+  `(minibuffer-prompt ((t (:foreground "#729fcf" :bold t))))
+  `(font-lock-warning-face ((t (:foreground "red" :bold t))))
+ 
+ '(header-line ((t (:box nil :foreground "grey90" :background "grey20" :inherit (mode-line))))) 
+)
+
+(provide-theme 'jqg)
