3bacea0b484ba1e1611313192713ff1bae093131
Shunsuke Tamiya <shunsuke.tamiya@legalforce.co.jp>
2021-04-25 12:15:35 +0900

    Remove slim from emacs config

--- vendor/packages.el
+++ vendor/packages.el
@@ -1,8 +1,6 @@
-(package-initialize)
 (require 'package)
 (add-to-list 'package-archives
-             '("MELPA Stable" . "http://stable.melpa.org/packages/") t)
-(package-initialize)
+	     '("MELPA Stable" . "http://stable.melpa.org/packages/") t)
 (if (not
      (fboundp 'flycheck-mode))
     (package-list-packages)
@@ -45,21 +43,21 @@
 ;; disable auto-loading
 (require 'flycheck)
 (setq-default flycheck-disabled-checkers
-              (append flycheck-disabled-checkers
-                      '(javascript-jshint)
-                      '(json-jsonlist)))
+	      (append flycheck-disabled-checkers
+		      '(javascript-jshint)
+		      '(json-jsonlist)))
 (flycheck-add-mode 'javascript-eslint 'web-mode)
 ;; flycheck prefix
 (setq-default flycheck-temp-prefix "~/.backups/.flycheck")
 (add-hook 'web-mode-hook (lambda ()
-                           (setq indent-tabs-mode nil)
-                           (setq web-mode-css-indent-offset 4)))
+			   (setq indent-tabs-mode nil)
+			   (setq web-mode-css-indent-offset 4)))
 
 ;; magit
 (require 'magit)
 (global-set-key (kbd "C-c p") 'magit-status)
 (global-set-key (kbd "C-c i")
-                (lambda() (interactive) (load-file "~/.emacs.d/init.el")))
+		(lambda() (interactive) (load-file "~/.emacs.d/init.el")))
 (define-key global-map (kbd "C-c g") 'magit-status)
 
 ;; configure with flake8 https://github.com/jorgenschaefer/elpy/wiki/Configuration
@@ -78,7 +76,7 @@
 (global-auto-complete-mode t)
 
 (setq ac-modes '(rust-mode go-mode ruby-mode shell-script-mode
-			   yaml-mode slim-mode web-mode
+			   yaml-mode web-mode
 			   cperl-mode c-mode))
 (editorconfig-mode 1)
 
@@ -86,6 +84,9 @@
 ;; (load-file "~/.emacs.d/vendor/prettier-js.el")
 ;; (require 'prettier-js)
 ;; (add-hook 'web-mode-hook 'prettier-js-mode)
+
+;; (add-to-list 'load-path "~/.emacs.d/vendor/o-blog")
+;; (require 'o-blog)
 
 (provide 'packages)
 ;;; packages.el ends here
