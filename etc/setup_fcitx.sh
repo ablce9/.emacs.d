bed88612471f880d1ff7bc78954f3ff485c727d4
Shunsuke Tamiya <shunsuke.tamiya@legalforce.co.jp>
2022-01-29 13:59:51 +0900

    Add tips for mozc setup under plasma env

--- etc/setup_fcitx.sh
+++ etc/setup_fcitx.sh
@@ -1,1 +1,3 @@
+# if doesn't work work, try below: https://askubuntu.com/questions/1242037/japanese-input-on-kubuntu-20-04-kcmodule-not-found
+# sudo apt install ibus-anthy ibus-gtk ibus-gtk3
 apt install fcitx fcitx-mozc fcitx-libpinyin kde-config-fcitx
